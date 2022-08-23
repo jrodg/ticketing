ced53ea7c165012ce66e41985c66b7c5a29cb7c6
juniorrodriguez <jrod.201@hotmail.com>
2022-08-23 11:08:28 -0400

    validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,30 @@
-import  express from 'express';
+import  express, {Request, Response} from 'express';
+import {body, validationResult} from 'express-validator'
+
 
 const router = express.Router();
 
-router.get('api/users/signup', (req, res) => {
-    res.send('Hello!')
+router.get('api/users/signup', [
+    body('email')
+    .isEmail()
+    .withMessage('Email most be valid'),
+    body('password')
+    .trim()
+    .isLength({ min: 4, max: 18})
+    .withMessage('Enter correct password')
+], 
+(req: Request, res: Response) => {
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(400).send(errors.array())
+    }
+    const {email, password} = req.body
+
+    console.log('Creating user .....')
+
+    res.send({})
 })
 
 export {router as signupRouter}
