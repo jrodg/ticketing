0e635bee972bb5e3dd820006890367de95ee6261
juniorrodriguez <jrod.201@hotmail.com>
2022-08-26 12:59:26 -0400

    signin-out-current-user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,11 @@
 import  express from 'express';
+import { currentUser } from '../middleware/current-user';
 
 const router = express.Router();
 
-router.get('api/users/currentusers', (req, res)=>{
-    res.send('Hello!')
+router.get("/api/users/currentuser", currentUser, (req, res)=>{
+    res.send({currentUser: req.currentUser || null})
+    
 })
 
 export {router as currentUserRouter}
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,10 +1,14 @@
 import  express , { Request, Response }from 'express';
 import { body } from "express-validator";
 import { validateRequest } from '../middleware/validate-request';
+import  jwt  from "jsonwebtoken";
+import { User } from "../models/user";
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
-router.post('api/users/signin', 
+router.post("/api/users/signin", 
 [
     body("email")
         .isEmail()
@@ -15,8 +19,31 @@ router.post('api/users/signin',
       .withMessage("Password most be entered")
 ], 
 validateRequest,
-(req: Request, res: Response) => {
+async(req: Request, res: Response) => {
+    const { email, password } = req.body;
     
+    const existingUser = await User.findOne({email})
+
+    if (!existingUser){
+      throw new BadRequestError("Invalid credentials");
+    }
+    const pswMatch = await Password.compare(existingUser.password, password)
+
+    if (!pswMatch) {
+        throw new BadRequestError("Invalid credentials");
+    }
+    //Generate jwt
+   
+    const userJWT = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email
+      }, process.env.JWT_KEY!)
+  
+      //Session storage
+      req.session = {
+        jwt: userJWT
+      }
+      res.status(200).send(existingUser)
 })
 
 export {router as signinRouter}
