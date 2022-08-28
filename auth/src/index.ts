388be982be1cbdca2e0b9f13a41b1e7ce73fcf8c
jrodg <jrod.201@hotmail.com>
2022-08-28 19:00:34 -0400

    jest

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,32 +1,4 @@
-import express from "express";
-import 'express-async-errors';
-import { json } from "body-parser";
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middleware/error-handler";
 import mongoose from "mongoose";
-import cookieSession = require("cookie-session") ;
-
-const app = express();
-app.set('trust proxy', true)
-app.use(json());
-app.use(cookieSession({
-    signed: false,
-    secure: true
-}))
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
-app.get('*',async (req,res) => {
-  throw new Error("");
-  
-})
-
-app.use(errorHandler)
 
 const start = async () => {
   if(!process.env.JWT_KEY){
