bf558d90b45c9310b726221273e4a30fec8f4547
jrodg <jrod.201@hotmail.com>
2022-08-29 23:12:05 -0400

    client

--- auth/src/test/jest.setup.ts
+++ auth/src/test/jest.setup.ts
@@ -1,10 +1,16 @@
 import { MongoMemoryServer  } from "mongodb-memory-server";
 import  mongoose  from "mongoose";
 import { app } from "../app";
+import request from 'supertest';
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = "asdasd"
+  jest.setTimeout(120000)
   mongo = await MongoMemoryServer.create()
   const mongoUri = await mongo.getUri();
 
@@ -14,6 +20,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+ jest.setTimeout(90000)
   const collections = await mongoose.connection.db.collections();
 
   for (let collection of collections) {
@@ -22,6 +29,23 @@ beforeEach(async () => {
 });
 
 afterAll(async () => {
+  jest.setTimeout(90000)
   await mongo.stop();
   await mongoose.connection.close();
 });
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
