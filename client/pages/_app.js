0209c20fcc543a1d68c08618961f234a51a6f6aa
juniorrodriguez <jrod.201@hotmail.com>
2022-10-12 16:12:17 -0400

    header nav

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,11 @@
 import "bootstrap/dist/css/bootstrap.css";
 import buildClient from "../api/build-client";
-const AppComponent = ({ Component, pageProps }) => {
+import Header from "../components/headers";
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
-      <h1>Header</h1>
+      <h1>Header </h1>
+      <Header currentUser={currentUser} />
       <Component {...pageProps} />
     </div>
   );
@@ -13,10 +15,17 @@ AppComponent.getInitialProps = async (appContext) => {
   //console.log(appContext)
   const client = buildClient(appContext.ctx);
   const { data } = await client.get("/api/users/currentuser");
-  const pgProps = await appContext.Component.getInitialProps(appContext.ctx);
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    const pgProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
   // console.log(pgProps)
   // console.log(data)
-  return data;
+  return {
+    pageProps,
+    ...data,
+  };
 };
 
 export default AppComponent;
